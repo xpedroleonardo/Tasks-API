0c41700945c492f95429272ed69fb5c0f92b4e7f
xpedroleonardo <aispedroleonardo@gmail.com>
2021-08-07 18:29:23 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import * as express from "express";
+import route from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(route);
+
+app.listen(3333, () => console.log("🚀 http://localhost:3333"));
